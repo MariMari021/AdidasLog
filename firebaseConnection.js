115e7a52839fd597cecbff3dcabf76034c5888be
MariMari021 <133662908+MariMari021@users.noreply.github.com>
2024-07-31 23:33:07 -0300

    Update firebaseConnection.js

--- firebaseConnection.js
+++ firebaseConnection.js
@@ -8,12 +8,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCDeUbTOqsAxoe-QIhoNVTlKsaUAG1UrBw",
-  authDomain: "msg-123-2bc77.firebaseapp.com",
-  projectId: "msg-123-2bc77",
-  storageBucket: "msg-123-2bc77.appspot.com",
-  messagingSenderId: "124212612710",
-  appId: "1:124212612710:web:bc19f1ab5144ced0b367a7"
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: 
 };
 
 // Inicializa o Firebase
